e9d05a1d3866813e2e84187728eecb05277c464d
Tolstikov Grigoriy <tolstikov.gn@edu.spbstu.ru>
2020-04-24 21:28:39 +0300

    Промежуточный коммит

---
+++ javalabs/labs/lab4/src/main/java/ru/spbstu/lab4/gui/Operations.java
@@ -0,0 +1,10 @@
+package ru.spbstu.lab4.gui;
+
+public enum Operations {
+    ADD,
+    EDIT,
+    DELETE,
+    FIND,
+    FILTER,
+    SHOW_ALL
+}
--- javalabs/labs/lab4/src/main/java/ru/spbstu/lab4/gui/controllers/ProductWindowController.java
+++ javalabs/labs/lab4/src/main/java/ru/spbstu/lab4/gui/controllers/ProductWindowController.java
@@ -1,9 +1,78 @@
 package ru.spbstu.lab4.gui.controllers;
 
-public class ProductWindowController {
-    MainWindowController parent;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.Label;
+import javafx.scene.control.TextField;
+import ru.spbstu.lab4.gui.Operations;
 
-    public void setParent(MainWindowController parent) {
+public class ProductWindowController {
+    @FXML
+    public Label fromLabel;
+    @FXML
+    public Label toLabel;
+    @FXML
+    public Label titleLabel;
+    @FXML
+    public Label priceLabel;
+    @FXML
+    public TextField titleText;
+    @FXML
+    private TextField toText;
+    @FXML
+    private TextField priceText;
+    @FXML
+    private TextField fromText;
+    @FXML
+    private Label typeOperLabel;
+
+    MainWindowController parent;
+    Operations operation;
+
+    public void setParentDetails(MainWindowController parent, Operations operation) {
         this.parent = parent;
+        this.operation = operation;
+
+        switch (operation) {
+            case ADD:
+                typeOperLabel.setText("ДОБАВЛЕНИЕ ПРОДУКТА");
+                titleText.setEditable(true);
+                priceText.setEditable(true);
+                break;
+            case EDIT:
+                typeOperLabel.setText("ИЗМЕНЕНИЕ ПРОДУКТА");
+                priceLabel.setText("НОВАЯ ЦЕНА");
+                titleText.setEditable(true);
+                priceText.setEditable(true);
+                break;
+            case DELETE:
+                typeOperLabel.setText("УДАЛЕНИЕ ПРОДУКТА");
+                titleText.setEditable(true);
+                priceText.setVisible(false);
+                priceLabel.setVisible(false);
+                break;
+            case FIND:
+                typeOperLabel.setText("ИЗМЕНЕНИЕ ПРОДУКТА");
+                titleText.setEditable(true);
+                priceText.setVisible(false);
+                priceLabel.setVisible(false);
+                break;
+            case FILTER:
+                typeOperLabel.setText("ФИЛЬТР ПО ЦЕНЕ");
+                titleLabel.setVisible(false);
+                titleText.setVisible(false);
+                priceText.setVisible(false);
+                priceLabel.setVisible(false);
+                toLabel.setVisible(true);
+                toText.setVisible(true);
+                fromLabel.setVisible(true);
+                fromLabel.setVisible(true);
+                break;
+        }
+    }
+
+    @FXML
+    private void clickOnButtonEvent(ActionEvent actionEvent) {
+        
     }
 }
